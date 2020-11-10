ea3f565e617990aae400c205ad3ab265191ddb1e
Zhengtong Liu <zhengtongliu@outlook.com>
2020-11-10 12:21:34 +0800

    modify Login and Signup pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Container, Grid, Button, Typography, 
-  makeStyles, CssBaseline, TextField, Link, Box } 
+import { Grid, Button, Typography, 
+  makeStyles, CssBaseline, Link, Box, InputAdornment, IconButton, OutlinedInput, InputLabel, FormControl, FormHelperText, TextField } 
   from "@material-ui/core";
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 const useStyles = makeStyles((theme) => ({
     page: {
       position: "absolute",
       padding: theme.spacing(3),
-      marginRight: "5%",
-      marginTop: "10%",
+      marginRight: "8%",
+      marginTop: "12%",
       marginLeft: "60%", 
       borderRadius: "30px",
       backgroundColor: "lightBlue"
@@ -23,41 +25,135 @@ const useStyles = makeStyles((theme) => ({
     form:{
       width: '100%',
       marginTop: theme.spacing(2),
+      
+    },
+
+    textField: {
+      margin: theme.spacing(2, 0, 2)
     },
 
     submit: {
-      margin: theme.spacing(3, 0, 2)
+      margin: theme.spacing(3, 0, 2),
+      width: "60%"
     }
 }));
+
+//spacing larger
+//confirm password, error -> box red, textfield attribute
+//button smaller
+
 const Login = () => {
   const classes = useStyles();
+
+  const [values, setValues] = React.useState({
+    email: '',
+    password: '',
+    showPassword: false
+  });
+  
+  const [errors, setErrors] = React.useState({})
+
+  
+
+  const handleChange = (prop) => (event) => {
+    setValues({...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const PasswordAdornment = () => {
+    return(
+      <InputAdornment position="end">
+        <IconButton
+          aria-label="toggle password visibility"
+          onClick={handleClickShowPassword}
+          onMouseDown={handleMouseDownPassword}
+          edge="end"
+          >
+          {values.showPassword ? <Visibility /> : <VisibilityOff />}
+          </IconButton>
+    </InputAdornment>
+    )
+  }
+
+  const validateEmail = (prop) => {
+    const expression = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
+    return expression.test(prop);
+  }
+
+  const validateNotEmpty = (prop) => {
+    const expression = /^.+$/;
+    return expression.test(prop);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    var validEmail = true;
+    var validPassword = true;
+    if(!validateEmail(values.email)) validEmail = false;
+    if(!validateNotEmpty(values.password)) validPassword = false;
+    setErrors({...errors,
+                email: (validEmail? '' : "This email address is invalid"),
+                password: (validPassword? '' : "This password is not valid")});
+  }
 
   return (
     <Box className={classes.page} maxWidth='xs'>
       <CssBaseline/>
         <div className={classes.paper}>
-          <form className={classes.form}>
-            <TextField
+          <form className={classes.form} onSubmit={handleSubmit} noValidate>
+            <FormControl
+            className={classes.textField}
             variant="outlined"
             margin="normal"
             required
             fullWidth
-            autoFocus
-            id="email"
-            label="Email Address"
             name="email"
-            type="email"
-            />
-            <TextField 
-            variant="outlined"
+            error={errors.email? true: false}
+            >
+              {console.log(errors.email)}
+              <InputLabel htmlFor="Email-Address">Email Address</InputLabel>
+              <OutlinedInput
+                autoFocus
+                id="email"
+                value={values.email}
+                onChange={handleChange("email")}
+                // type="email"
+                labelWidth={112}
+              />
+              <FormHelperText>{errors.email}</FormHelperText>
+            </FormControl>
+
+            <FormControl 
+            className={classes.textField} 
+            variant="outlined" 
             margin="normal"
-            required
+            required 
             fullWidth
-            id="password"
-            label="Password"
-            name="password"
-            type="password"
-            />
+            error={errors.password? true: false}
+            name="password">
+              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <OutlinedInput
+              id="password"
+              value={values.password}
+              onChange={handleChange("password")}
+              type={values.showPassword ? 'text' : 'password'}
+              endAdornment={
+                <PasswordAdornment>
+                </PasswordAdornment>
+              }
+              labelWidth={80}
+              />
+              <FormHelperText>{errors.password}</FormHelperText>
+            </FormControl>
+              
+            
 
           <Grid item>
             <Typography align="center">
@@ -67,16 +163,21 @@ const Login = () => {
             </Typography>
           </Grid>
 
-          <Button
-            className={classes.submit}
-            color="primary"
-            variant="contained"
-            fullWidth
-            type="submit"
-            //return form back to the server
-          >
-            Log in
-          </Button>
+          <Grid item>
+            <Typography align="center">
+              <Button
+              className={classes.submit}
+              color="primary"
+              variant="contained"
+              fullWidth
+              type="submit"
+              
+              //return form back to the server
+            >
+              Log in
+            </Button>
+            </Typography>
+          </Grid>
           </form>
         </div>
     </Box>
