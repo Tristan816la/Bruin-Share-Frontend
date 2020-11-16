d5dd83d10331cfa4363e9fff8f20155796f93ae5
Olivia <zhtyolivia@gmail.com>
2020-11-16 15:50:13 +0800

    added private navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,27 +15,56 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function Navbar() {
-  return (
-    <nav className="navbar">
-      <div className="navbar__left">
+  const loggedIn = false; 
+  if (!loggedIn) {
+    return (
+      <nav className="navbar">
+        <div className="navbar__left">
+          <img
+            src="logo.png"
+            alt=""
+          />
+          <Link to="/">
+            <div className="bruinShare">BruinShare</div>
+          </Link>
+        </div>
+        <div className="navbar__right">
+          <Link to="/Signup">
+            <div className="navbar__option">Sign Up</div>
+          </Link>
+          <Link to="/Login">
+            <div className="navbar__option" id="navbar__option2">Log In</div>
+          </Link>
+        </div>
+      </nav>
+    );
+  } else {
+    return (
+      <nav className="navbar">
+        <div className="navbar__left">
         <img
-          src="logo.png"
-          alt=""
-        />
-        <Link to="/">
-          <div className="bruinShare">BruinShare</div>
-        </Link>
-      </div>
-      <ul className="navbr__right">
-        <Link to="/Signup">
-          <li className="navbar__option">Sign Up</li>
-        </Link>
-        <Link to="/Login">
-          <li className="navbar__option" id="navbar__option2">Log In</li>
-        </Link>
-      </ul>
-    </nav>
-  );
+            src="logo.png"
+            alt=""
+          />
+          <Link to="/">
+            <div className="bruinShare">BruinShare</div>
+          </Link>
+        </div>
+
+        <div className="loggedin__right">
+          <NotificationsIcon className='notification'/>
+          <Link to="/Home">
+            <div className="navbar__option">Sign Out</div>
+          </Link>
+          <Link to="/Private">
+            <Avatar className="avatar"/>
+          </Link>
+        </div>
+      </nav>
+
+    )
+  }
+  
 }
 
 export default Navbar;
