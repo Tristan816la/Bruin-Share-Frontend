67ea8914615901bdfe97063cac316ed5b0f37d5b
Zhengtong Liu <zhengtongliu@outlook.com>
2020-11-17 09:44:21 +0800

    update at local: Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Navbar from "../components/Navbar/Navbar";
-
+import axios from "axios";
 import MainStoryBox from "../components/MainStoryBox";
-import { makeStyle, makeStyles} from "@material-ui/core";
-import SideBar from "../components/SideBar/SideBar"
+import { makeStyles, Box } from "@material-ui/core";
+import SideBar from "../components/SideBar/SideBar";
+import { useHistory } from "react-router-dom";
 
 const useStyle = makeStyles((theme) => ({
-  temp: {
+  root: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  body: {
     display: "flex",
     flexDirection: "row",
-    margin: theme.spacing(1),
-    "& > *": {
-      margin: theme.spacing(1),
-    },
-  }
+  },
+  sideBar: {
+  },
+
+  posts: {
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr 1fr",
+    padding: theme.spacing(1, 2, 2),
+    gridColumnGap: "1em",
+    gridRowGap: "1em",
+    overflow: "scroll",
+    height: "830px",
+  },
 }));
 
 
@@ -23,37 +36,47 @@ const Home = () => {
   const signoutaction = () => {
     window.localStorage.removeItem("AuthToken");
   };
-  const name = "Tom";
-  const title = "this is a title";
-  const message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \
-  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \
-  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo \
-  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse \
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \
-  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim";
-  const time = "15 Oct, 2020";
+
+  
+  
+  const [posts, setPosts] = React.useState({
+
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("/publicposts")
+    .then(res => res.data);
+      setPosts(result);
+    }
+    fetchData();
+  }, []);
+  
+  console.log(posts);
+  
+ 
+
   const classes = useStyle();
   return (
-    <div>
+    <div className={classes.root}>
       <Navbar></Navbar>
 
 
       {/* <h1>Home</h1> */}
       {/* <button onClick={signoutaction}>Sign out</button> */}
-      <div className="home_body">
-        <SideBar />
-        {/*<Post />*/}
-
-      </div>
-      <div className={classes.temp}>
-      <MainStoryBox name={name} title={title} 
-      content={message}
-      time={time}>
-      </MainStoryBox>
-      <MainStoryBox name={name} title={title} 
-      content={message}
-      time={time}>
-      </MainStoryBox>
+      <div className={classes.body}>
+        <Box><SideBar className={classes.sideBar} /></Box>
+        
+        {posts.length && <Box className={classes.posts}>
+            {posts.map((post, i) => <MainStoryBox 
+            key={i}
+            name={post.postBy.name}
+            title={post.topic}
+            content={post.content}
+            time={post.updatedAt}
+            />)}
+            
+      </Box> }
       </div>
     </div>
   );
