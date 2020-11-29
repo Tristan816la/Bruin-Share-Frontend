f82dbfa03be8eb751e0833ed58cac2ec2dc3f808
Tristan816la <tristanq816@gmail.com>
2020-11-29 11:01:14 -0800

    added options for search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
+import { Typography } from "@material-ui/core";
 
 function SearchBar() {
+  const [option, setOption] = useState("Search Content...");
+
+  const handleOption = (e) => {
+    if (e.target.outerText === "Topic") setOption("Search Topic...");
+    else setOption("Search Content...");
+  };
 
   return (
     <div>
@@ -9,19 +16,25 @@ function SearchBar() {
         <div className="drop_down">
           <button id="drop_down_button">▼</button>
           <div className="dropdown_content">
-            <a href="#">Article</a>
-            <a href="#">User</a>
+            <Typography onClick={handleOption}>Topic</Typography>
+            <Typography onClick={handleOption}>Content</Typography>
           </div>
         </div>
 
         <div className="search_bar">
-          <input type="text" placeholder="Search stories..." />
+          <input type="text" placeholder={option} />
         </div>
       </div>
       <div className="box">
-        <img type="submit" src="search_icon.png" id="search" onClick="searchTopic()" />
+        <img
+          type="submit"
+          src="search_icon.png"
+          id="search"
+          onClick="searchTopic()"
+          alt="search"
+        />
       </div>
-    </div >
+    </div>
   );
 }
 
