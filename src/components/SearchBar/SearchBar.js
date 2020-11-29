b66036fb83d0b856dd23477b20bbb61d52f3324e
Hongye Li <lishuyue421@163.com>
2020-11-29 09:32:10 +0900

    search box styling

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,20 +2,26 @@ import React from "react";
 import "./SearchBar.css";
 
 function SearchBar() {
+
   return (
     <div>
       <div className="search">
         <div className="drop_down">
-          <button>Type</button>
+          <button id="drop_down_button">▼</button>
+          <div className="dropdown_content">
+            <a href="#">Article</a>
+            <a href="#">User</a>
+          </div>
         </div>
+
         <div className="search_bar">
           <input type="text" placeholder="Search stories..." />
         </div>
       </div>
       <div className="box">
-        <img src="search_icon.png" alt="search" id="search" />
+        <img type="submit" src="search_icon.png" id="search" onClick="searchTopic()" />
       </div>
-    </div>
+    </div >
   );
 }
 
