c618d803b7fd289ae54fbc7df5db9d6c9d43fa1b
Tristan816la <tristanq816@gmail.com>
2020-11-29 11:17:37 -0800

    fix private page error

--- src/utils/useStyles.js
+++ src/utils/useStyles.js
@@ -222,14 +222,12 @@ export const useStyles = makeStyles((theme) => ({
     color: "#666",
     display: "flex",
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
   itemText: {
-    cursor: "pointer"
+    cursor: "pointer",
   },
-  deleteButton: {
-
-  },
+  deleteButton: {},
 
   /**<------------------------- Profile ------------------------------> */
   profilebody: {
@@ -239,7 +237,7 @@ export const useStyles = makeStyles((theme) => ({
     gridColumnGap: "10vw",
   },
   profileSideBar: {
-    position: "relative"
+    position: "relative",
   },
   profileposts: {
     position: "relative",
@@ -257,9 +255,8 @@ export const useStyles = makeStyles((theme) => ({
   },
   privateposts: {
     position: "relative",
-    top: "10vh",
     display: "grid",
+    width: "41vw",
     marginTop: "10vh",
-
   },
 }));
